0ea16e7ca1ef2267ac1f13d807475c9d417443b5
sergio <sergiojrg98@gmail.com>
2022-02-15 16:41:42 -0600

    imagen return

--- applications/equipos/models.py
+++ applications/equipos/models.py
@@ -9,6 +9,7 @@ class EquiposModel(models.Model):
     telefono = PhoneNumberField(unique = True, null = False, blank = False)
     ingeniero = models.CharField('Nombre del Ingeniero',max_length=120)
     serie = models.CharField('Número de Serie',max_length=80)
+    imagen = models.ImageField('Imagen',upload_to='EquiposIMG',blank=False,null=False)  
     imagen = CloudinaryField('Imagen',null=True,blank=True)
     duracion = models.CharField('Duración de la Póliza',max_length=100, blank=True)
     caducidad = models.CharField('Fecha de caducidad de la Póliza',max_length=100,blank=True)
