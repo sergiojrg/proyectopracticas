d4a44f509393da70685f0b2f20729a68656697b6
sergio <sergiojrg98@gmail.com>
2022-02-15 11:29:53 -0600

    ajuste-settings

--- imax/settings/local.py
+++ imax/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['practicas-prof.herokuapp.com']
+ALLOWED_HOSTS = ['equiposimax.herokuapp.com/']
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
