5e02453efb6d9b72d5a9e71d9a4b31b4198987d4
sergio <sergiojrg98@gmail.com>
2022-02-15 12:39:16 -0600

    static changes

--- imax/settings/local.py
+++ imax/settings/local.py
@@ -24,8 +24,11 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['static/']
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = '/static/'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = ("media")
