03b3fa6c9db8b521938e1ef65b923d6a40f0a75a
sergio <sergiojrg98@gmail.com>
2022-02-15 12:31:38 -0600

    static_root

--- imax/settings/local.py
+++ imax/settings/local.py
@@ -24,7 +24,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['static/']
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
